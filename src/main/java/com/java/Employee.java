39770ced5da81e4f36a977a470a3ff59619532a6
alikeli <eli-com2@yandex.ru>
2023-01-23 19:10:20 +0100

    this, constructor

--- src/main/java/com/java/Employee.java
+++ src/main/java/com/java/Employee.java
@@ -6,6 +6,7 @@ public class Employee {
     int age;
     double salary;
     String department;
+    String surname;
 
     int count;
 
@@ -21,11 +22,10 @@ public class Employee {
         department = this.department;
     }
 
-    public Employee(String name, int age, String department) {
+    public Employee(String name, int age, double salary, String department, String surname) {
+        this(name, age, salary, department);
+        surname = this.surname;
         count = ++id;
-        age = this.age;
-        name = this.name;
-        department = this.department;
     }
 
 
@@ -34,7 +34,7 @@ public class Employee {
 class EmployeeTest {
     public static void main(String[] args) {
         Employee emp = new Employee("Liza", 25, 2500, "it");
-        Employee emp1= new Employee("Liza", 25, "it");
+        Employee emp1 = new Employee("Liza", 25, 2800, "hr", "Ivanova" );
 
         System.out.println(emp.getId());
     }

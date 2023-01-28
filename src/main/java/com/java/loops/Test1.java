dfc1c08da7bfdfbba053856166e54dfbf4f2fa84
alikeli <eli-com2@yandex.ru>
2023-01-28 18:35:06 +0100

    loops

---
+++ src/main/java/com/java/loops/Test1.java
@@ -0,0 +1,11 @@
+package com.java.loops;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int a = 15;
+        if (a > 10)
+            for (int i = 1; i < 15; i++)
+                System.out.println(i);
+        System.out.println("loop ended");
+    }
+}

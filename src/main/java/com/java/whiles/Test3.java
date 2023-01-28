113ab7d5aa2a779099830d0eff6d78d224398910
alikeli <eli-com2@yandex.ru>
2023-01-28 19:16:46 +0100

    while

---
+++ src/main/java/com/java/whiles/Test3.java
@@ -0,0 +1,19 @@
+package com.java.whiles;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i++ < 10) {
+            i++;
+        }
+        System.out.println(i);
+
+
+        int a = 5;
+        while (a < 10) {
+
+            System.out.println(a);
+            a++;
+        }
+    }
+}

f4dca00b50dd959e41ba62b34312321b597bf5c7
alikeli <eli-com2@yandex.ru>
2023-01-28 19:18:51 +0100

    do while

---
+++ src/main/java/com/java/whiles/Test4.java
@@ -0,0 +1,12 @@
+package com.java.whiles;
+
+public class Test4 {
+    public static void main(String[] args) {
+        int a = 10;
+        do {
+            System.out.println("privet  " + a);
+            a++;
+        } while (a < 20);
+
+    }
+}

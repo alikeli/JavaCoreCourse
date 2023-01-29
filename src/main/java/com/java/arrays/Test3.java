aee4168c3f69367edd2c7a325aed53243d4ddb7c
alikeli <eli-com2@yandex.ru>
2023-01-29 20:30:29 +0100

    Arrays

---
+++ src/main/java/com/java/arrays/Test3.java
@@ -0,0 +1,26 @@
+package com.java.arrays;
+
+public class Test3 {
+    public static void maxMin(double[] array) {
+        double max = array[0];
+        double min = array[0];
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > max) {
+                max = array[i];
+
+            }
+
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("Min = " + min + ". Max = " + max);
+
+    }
+
+    public static void main(String[] args) {
+        double[] array1 = {1, 10.5, 88.9, -100, -88,9, 55, 12 };
+        maxMin(array1);
+    }
+}

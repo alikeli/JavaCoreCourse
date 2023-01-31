7e99a3dc04bd7335ed2f6f9632e9c417461023f0
alikeli <eli-com2@yandex.ru>
2023-01-31 12:16:02 +0100

    number 4

---
+++ src/main/java/com/java/number4.java
@@ -0,0 +1,17 @@
+package com.java;
+
+public class number4 {
+    public static void main(String[] args) {
+        String s = "Hello, world";
+        String[] worlds = s.split(", ");
+        for (String world : worlds) {
+            System.out.println(world);
+        }
+
+
+    }
+
+
+}
+
+

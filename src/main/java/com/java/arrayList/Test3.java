12650a162802a94207b3f4dde71532dc4a647d62
alikeli <eli-com2@yandex.ru>
2023-02-07 19:06:25 +0100

    iterator

--- src/main/java/com/java/arrayList/Test3.java
+++ src/main/java/com/java/arrayList/Test3.java
@@ -1,6 +1,7 @@
 package com.java.arrayList;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Test3 {
     public static void main(String[] args) {
@@ -10,11 +11,23 @@ public class Test3 {
         list.add("Ivan");
         list.add("Olga");
 
-        System.out.println(list.toString());
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("item  = " + iterator.next());
+        }
 
-        ArrayList<String> list2 = (ArrayList<String>) list.clone();
-        System.out.println(list2 == list);
-        System.out.println(list2.get(0) == list.get(0));
+        Iterator<String> iterator1 = list.iterator();
+        while (iterator1.hasNext()) {
+            iterator1.next();
+            iterator1.remove();
+        }
+        System.out.println(list);
+
+//        System.out.println(list.toString());
+//
+//        ArrayList<String> list2 = (ArrayList<String>) list.clone();
+//        System.out.println(list2 == list);
+//        System.out.println(list2.get(0) == list.get(0));
 
     }
 }
